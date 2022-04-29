18bfe17a92d320c3dd2384dc180812545a892bc8
darayaq <61315184+darayaq-01@users.noreply.github.com>
2022-04-29 12:19:59 +0200

    delete:
    extra button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,10 +12,6 @@ const Header = () => {
           Dogs also need t-shirts!
         </h2>
         <h3 className='text-2xl mb-8 text-gray-200'>Some random nice pics!</h3>
-
-        <button className='bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider'>
-          Pre Order
-        </button>
       </div>
     </section>
   );
